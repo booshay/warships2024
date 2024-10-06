97443206e98c245e9aabf25fabf14c1289e11d46
booshay <boucher_08641@yahoo.com>
2024-10-06 18:13:45 -0400

    added rest of the components

---
+++ src/app/relics/relics.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-relics',
+  standalone: true,
+  imports: [],
+  templateUrl: './relics.component.html',
+  styleUrl: './relics.component.css'
+})
+export class RelicsComponent {
+
+}
---
+++ src/app/rss/rss.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rss',
+  standalone: true,
+  imports: [],
+  templateUrl: './rss.component.html',
+  styleUrl: './rss.component.css'
+})
+export class RssComponent {
+
+}
