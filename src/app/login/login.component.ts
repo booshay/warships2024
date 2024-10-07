850826134f9a86949e7b61f95ea0ec0978c7cf21
booshay <boucher_08641@yahoo.com>
2024-10-06 20:27:52 -0400

    completed logon form.  Login actually works

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,45 @@
 import { Component } from '@angular/core';
+import { JwtAuthService } from '../jwt-auth.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder, public jwtAuth: JwtAuthService
+  ) {
+    this.loginForm = this.fb.group({
+      username: '',
+      password: ''
+    });
+  }
+
+  user = {
+    username: '',
+    password: ''
+  };
+
+  login() {
+    const formValue = this.loginForm.value;
+    this.jwtAuth.login(formValue.username, formValue.password)
+      .subscribe(user => {
+        //
+      },
+        (error) => {
+          //this.messageService.showError(JSON.stringify(error.error.error), 'Error');
+          this.loginForm.reset();
+        }
+      )
+  }
 
 }
