9fbe7ea6db10893252f384ee02fd5281047d8b38
booshay <boucher_08641@yahoo.com>
2024-10-08 19:32:36 -0400

    finished importing everything for the mines component.  Data is not coming in though

--- src/app/mines/mines.component.ts
+++ src/app/mines/mines.component.ts
@@ -4,14 +4,18 @@ import { PsqlService } from '../psql.service';
 import { MessageService } from '../message.service';
 import { Router } from '@angular/router';
 import { JwtAuthService } from '../jwt-auth.service';
+import { User } from '../_models/user';
 
 //material
 import { MatSort, MatSortModule } from '@angular/material/sort';
-import { MatTableDataSource } from '@angular/material/table';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatSelect, MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 //forms
 import { FormsModule } from '@angular/forms';
@@ -22,7 +26,7 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-mines',
   standalone: true,
-  imports: [NavbarComponent, FormsModule, ReactiveFormsModule, MatSelectModule, MatPaginatorModule, MatSortModule, CommonModule, MatFormFieldModule, MatInputModule],
+  imports: [NavbarComponent, FormsModule, ReactiveFormsModule, MatIconModule, MatTableModule, MatButtonModule, MatRadioModule, MatSelectModule, MatPaginatorModule, MatSortModule, CommonModule, MatFormFieldModule, MatInputModule],
   templateUrl: './mines.component.html',
   styleUrl: './mines.component.css'
 })
@@ -36,35 +40,74 @@ export class MinesComponent implements OnInit {
       y: null,
       enhanced: '0'
     });
+
+    this.rutileForm = this.fb.group({
+      x: null,
+      y: null
+    });
+
+    this.zoneForm = this.fb.group({
+      z: null
+    });
+    this.myForm.controls['enhanced'].setValue('0');
   }
 
 
   myForm: FormGroup;
+  rutileForm: FormGroup;
+  zoneForm: FormGroup;
+
+  public displayedColumns = ['lvl', 'type', 'x', 'y', 'enhanced', 'delete'];
+  public dataSource = new MatTableDataSource<any>();
   levels = ['All', 44, 46, 48, 50];
   levelsSelect = ['44', '46', '48', '50'];
   enhancements = ['0', '50', '100', '150', '200'];
   tileTypes = ['Gold', 'Iron', 'Oil', 'Copper', 'Uranium'];
   coordCheckResult: {}
   isAdmin: Boolean;
-  user: {};
+  user: User;
+  lvlvalue: number;
+  enhanced: number;
+  typevalue: string;
+  temp = '';
+  zones = [
+    { z: 1, x: 0, y: 401 },
+    { z: 2, x: 201, y: 401 },
+    { z: 3, x: 401, y: 401 },
+    { z: 4, x: 0, y: 201 },
+    { z: 5, x: 201, y: 201 },
+    { z: 6, x: 401, y: 201 },
+    { z: 7, x: 0, y: 0 },
+    { z: 8, x: 201, y: 0 },
+    { z: 9, x: 401, y: 0 }
+  ]
+
 
   @ViewChild('xRef') nameElementRef: ElementRef;
   @ViewChild('yRef') nameElementRefy: ElementRef;
   @ViewChild('nameRef2') nameElementRef2: MatSelect;
   @ViewChild('nameRef3') nameElementRef3: MatSelect;
 
-
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
 
   ngOnInit(): void {
-
+    this.auth.currentUser.subscribe(data => {
+      this.user = data;
+      this.isAdmin = data.user == "admin"
+      this.psqlService.getCoords("mines", this.user)
+        .subscribe(mine => {
+          console.log(mine)
+          this.dataSource.data = mine;
+          this.dataSource.sort = this.sort;
+        });
+    })
   }
   onKeydown(event) {
     if (event.key === "ArrowRight") {
       this.nameElementRefy.nativeElement.focus();
     }
   }
-
-
 
   addMine() {
     const formValue = this.myForm.value;
@@ -100,14 +143,10 @@ export class MinesComponent implements OnInit {
     }
   }
 
-
-
-
-  deleteMine(id) {
+  deleteMine(id: number): void {
     this.psqlService.deleteCoord("mines", id).subscribe(() => {
       this.ngOnInit();
     })
-
   }
 
   updateMine(id, enhancement) {
@@ -158,5 +197,53 @@ export class MinesComponent implements OnInit {
       })
     }
   }
+
+  applyRadioFilter(filterValue: string | number): void {
+    this.lvlvalue = Number(filterValue);
+    this.dataSource.filterPredicate = (data, filter: string) => {
+      return data.lvl == filter;
+    };
+    if (filterValue === 'All') {
+      filterValue = '';
+    }
+    this.dataSource.filter = filterValue.toString().trim().toLowerCase();
+    console.log(this.dataSource.filter)
+  }
+
+  applyRadioFilter2(filterValue: string) {
+    this.dataSource.filterPredicate = (data, filter: string) => {
+      return data.type == filter && data.lvl == this.lvlvalue;
+    };
+    //if (filterValue === 'All') {
+    //filterValue = '';
+    //}
+    this.dataSource.filter = filterValue.toString();
+    console.log(this.dataSource.filter)
+  }
+
+  rutileFilter() {
+    const formValue = this.rutileForm.value;
+    this.dataSource.filterPredicate = (data, filter: string) => {
+      return Number(data.x) > Number(filter['x']) && Number(data.x) < Number(filter['x']) + 100 && Number(data.y) > Number(filter['y'])
+        && Number(data.y) < Number(filter['y']) + 100;
+    };
+    this.dataSource.filter = formValue; // x&Y of lower left
+  }
+
+  zoneFilter() {  //    { z: 4, x: 0, y: 201 }
+    const formValue = this.zoneForm.value;
+    this.dataSource.filterPredicate = (data, filter: string) => {
+      const zone = this.zones.find(({ z }) => z === Number(filter['z']));
+      return Number(data.x) > Number(zone['x']) && Number(data.x) < Number(zone['x']) + 200 && Number(data.y) > Number(zone['y'])
+        && Number(data.y) < Number(zone['y']) + 200;
+    };
+    if (formValue.z >= 0 && formValue.z <= 9) {
+      this.dataSource.filter = formValue;
+    }
+    else {
+      this.messageService.showError('Please enter a number 0-9', 'Error')
+    }
+  }
+
 
 }
