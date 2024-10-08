229adb06ce12943f4f41b271b550d760f4092d50
booshay <boucher_08641@yahoo.com>
2024-10-08 10:16:14 -0400

    psql service all updated to work in angular18.  Inteceptor finished

--- src/app/psql.service.ts
+++ src/app/psql.service.ts
@@ -14,11 +14,6 @@ export class PsqlService {
     return this.http.get(this.baseUrl)
   }
 
-  /*   getCoords(type, user): Observable<any> {
-      console.log(this.baseUrl + "get" + type, user)
-      return this.http.post<any>(this.baseUrl + "get" + type, user)
-    } */
-
   getCoords(type: string, user: any): Observable<any> {
     const body = { type, user }; // Structure the request body
     console.log(`${this.baseUrl}get${type}`, body); // Logging for debugging
@@ -26,6 +21,40 @@ export class PsqlService {
     return this.http.post<any>(`${this.baseUrl}get${type}`, body); // Send the structured body
   }
 
+  addCoord(type: string, data: any, user: any): Observable<any> {
+    const body = { ...data, user }; // Attach user to data object
+    return this.http.post<any>(`${this.baseUrl}${type}`, body); // Post structured data
+  }
+
+  deleteCoord(type: string, id: number): Observable<any> {
+    return this.http.delete<any>(`${this.baseUrl}${type}/${id}`); // Direct delete
+  }
+
+  updateCoord(type: string, id: number, enhancement: any): Observable<any> {
+    const body = { enhanced: enhancement }; // Structured body for update
+    return this.http.put<any>(`${this.baseUrl}${type}/${id}`, body); // Put request
+  }
+
+  runQuery(query: string): Observable<any> {
+    const body = { query }; // Wrap query string in body
+    return this.http.post<any>(`${this.baseUrl}query`, body); // Post query
+  }
+
+  coordCheck(coords: any): Observable<any> {
+    const body = { position: coords }; // Structured body for coordCheck
+    return this.http.post<any>(`${this.baseUrl}coordCheck`, body); // Post request
+  }
+
+  checkIfCoordExists(type: string, position: any, user: any): Observable<any> {
+    const body = { position, user }; // Structured body for duplicate check
+    console.log(`${this.baseUrl}${type}check`, body);
+    return this.http.post<any>(`${this.baseUrl}${type}check`, body); // Post request
+  }
+
+  /*   getCoords(type, user): Observable<any> {
+    console.log(this.baseUrl + "get" + type, user)
+    return this.http.post<any>(this.baseUrl + "get" + type, user)
+  } 
 
   addCoord(type, data, user) {
     data.user = user
@@ -57,5 +86,5 @@ export class PsqlService {
     console.log(this.baseUrl + type + 'check', data)
 
     return this.http.post<any>(this.baseUrl + type + 'check', data);
-  }
+  } */
 }
